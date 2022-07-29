0b6fb1473a03c74683514e2151197d4893b4cc3e
Ian Baker <inbox@ianbaker.me>
2022-07-29 22:26:08 +0100

    Add project files.

---
+++ MarsRover.Tests/UnitTest1.cs
@@ -0,0 +1,197 @@
+namespace MarsRover.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Plateau_ShouldInit_WhenValidDimensionsPassed()
+        {
+            // Arrange
+            var x = 10;
+            var y = 20;
+
+            // Act
+            var sut = new Plateau(x, y);
+
+            // Assert
+            Assert.That(sut.x, Is.EqualTo(x));
+            Assert.That(sut.y, Is.EqualTo(y));
+        }
+
+        [Test]
+        public void Plateau_ShouldUpdateCurrentPos_WhenPositionSet()
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+
+            // Act
+            plateau.SetPosition(1, 3);
+
+            // Assert
+            Assert.That(plateau.GetPosition().x, Is.EqualTo(1));
+            Assert.That(plateau.GetPosition().y, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Plateau_ShouldIncrementY_WhenMoveNorthCalled()
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+            plateau.SetPosition(1, 3);
+
+            // Act
+            plateau.MoveNorth();
+
+            // Assert
+            Assert.That(plateau.GetPosition().x, Is.EqualTo(1));
+            Assert.That(plateau.GetPosition().y, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Plateau_ShouldIncrementX_WhenMoveEastCalled()
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+            plateau.SetPosition(1, 3);
+
+            // Act
+            plateau.MoveEast();
+
+            // Assert
+            Assert.That(plateau.GetPosition().x, Is.EqualTo(2));
+            Assert.That(plateau.GetPosition().y, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Plateau_ShouldDecrementY_WhenMoveSouthCalled()
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+            plateau.SetPosition(1, 3);
+
+            // Act
+            plateau.MoveSouth();
+
+            // Assert
+            Assert.That(plateau.GetPosition().x, Is.EqualTo(1));
+            Assert.That(plateau.GetPosition().y, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Plateau_ShouldDecrementX_WhenMoveWestCalled()
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+            plateau.SetPosition(1, 3);
+
+            // Act
+            plateau.MoveWest();
+
+            // Assert
+            Assert.That(plateau.GetPosition().x, Is.EqualTo(0));
+            Assert.That(plateau.GetPosition().y, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Plateau_ShouldNotBeAbleToMoveOffThePlateau_WhenMoveNorthCalled()
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+            plateau.SetPosition(10, 20);
+
+            // Act
+            plateau.MoveNorth();
+
+            // Assert
+            Assert.That(plateau.GetPosition().x, Is.EqualTo(10));
+            Assert.That(plateau.GetPosition().y, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void Plateau_ShouldNotBeAbleToMoveOffThePlateau_WhenMoveEastCalled()
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+            plateau.SetPosition(10, 20);
+
+            // Act
+            plateau.MoveEast();
+
+            // Assert
+            Assert.That(plateau.GetPosition().x, Is.EqualTo(10));
+            Assert.That(plateau.GetPosition().y, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void Plateau_ShouldNotBeAbleToMoveOffThePlateau_WhenMoveSouthCalled()
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+            plateau.SetPosition(0, 0);
+
+            // Act
+            plateau.MoveSouth();
+
+            // Assert
+            Assert.That(plateau.GetPosition().x, Is.EqualTo(0));
+            Assert.That(plateau.GetPosition().y, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Plateau_ShouldNotBeAbleToMoveOffThePlateau_WhenMoveWestCalled()
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+            plateau.SetPosition(0, 0);
+
+            // Act
+            plateau.MoveWest();
+
+            // Assert
+            Assert.That(plateau.GetPosition().x, Is.EqualTo(0));
+            Assert.That(plateau.GetPosition().y, Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase("N", ExpectedResult = "N")]
+        [TestCase("E", ExpectedResult = "E")]
+        [TestCase("S", ExpectedResult = "S")]
+        [TestCase("W", ExpectedResult = "W")]
+        public string Plateau_ShouldUpdateCurrentDirection_WhenDirectionSet(string orientation)
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+            plateau.SetPosition(0, 0);
+
+            // Act
+            plateau.SetDirection(orientation);
+
+            // Assert
+            return plateau.GetDirection();
+        }
+
+        [Test]
+        [TestCase(1, 3, "N", 1, 4)]
+        [TestCase(1, 3, "E", 2, 3)]
+        [TestCase(1, 3, "S", 1, 2)]
+        [TestCase(1, 3, "W", 0, 3)]
+        public void Plateau_ShouldMoveInTheRightDirection_WhenDirectionSet(int startX, int startY, string orientation, int expectedX, int expectedY)
+        {
+            // Arrange
+            var plateau = new Plateau(10, 20);
+            plateau.SetPosition(startX, startY);
+            plateau.SetDirection(orientation);
+
+            // Act
+            plateau.Move();
+
+            // Assert
+            Assert.That(plateau.GetPosition().x, Is.EqualTo(expectedX));
+            Assert.That(plateau.GetPosition().y, Is.EqualTo(expectedY));
+        }
+    }
+}
---
+++ MarsRover/Plateau.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Plateau
+    {
+        private int currentX;
+        private int currentY;
+        private string heading;
+
+        public Plateau(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public void SetPosition(int x, int y)
+        {
+            this.currentX = x;
+            this.currentY = y;
+        }
+
+        public (int x, int y) GetPosition()
+        {
+            return (currentX, currentY);
+        }
+
+        public void SetDirection(string heading)
+        {
+            this.heading = heading;
+        }
+
+        public string GetDirection()
+        {
+            return this.heading;
+        }
+
+        public void MoveNorth()
+        {
+            if (this.currentY < this.y)
+                this.currentY++;
+        }
+
+        public void MoveEast()
+        {
+            if (this.currentX < this.x)
+                this.currentX++;
+        }
+
+        public void MoveSouth() 
+        { 
+            if (this.currentY > 0)
+                this.currentY--; 
+        }
+
+        public void MoveWest() 
+        { 
+            if (this.currentX > 0)
+                this.currentX--; 
+        }
+
+        public void Move()
+        {
+            switch (this.GetDirection())
+            {
+                case "N":
+                    this.MoveNorth();
+                    break;
+                case "E":
+                    this.MoveEast();
+                    break;
+                case "S":
+                    this.MoveSouth();
+                    break;
+                case "W":
+                    this.MoveWest();
+                    break;
+            }
+        }
+    }
+}
